8e0149b92b03d499c8497946a9af9b44a1834903
Henrik Holst <hholst80@users.noreply.github.com>
2017-01-21 13:35:51 +0100

    Delete symlink-dotfiles.sh

--- symlink-dotfiles.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-VIMHOME=${HOME}/.vim
-for FILE in gitconfig vimrc tmux.conf redshift.conf Xresources bash_aliases toprc
-do
-	TARGET=${HOME}/.${FILE}
-	if ! test -s ${TARGET}; then
-		ln -s -v ${VIMHOME}/${FILE} ${HOME}/.${FILE}
-	fi
-done
